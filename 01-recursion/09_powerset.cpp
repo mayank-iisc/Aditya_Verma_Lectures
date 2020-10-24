c5ca9da443b1670ef2af45ac4b26ec9280537f69
Mayank Tiwari <tmayanc@gmail.com>
2020-10-24 14:47:10 +0530

    Update 09_powerset.cpp

--- 01-recursion/09_powerset.cpp
+++ 01-recursion/09_powerset.cpp
@@ -5,19 +5,19 @@ using namespace std;
 
 void solve(string ip, string op){
 	
-	if(ip.length() == 0){
+	if(ip.length() == 0){				//Base Condition
 		cout << op << " ";
 		return;
 	}
 	
-	string op1 = op;
+	string op1 = op;				//Now we are simply following the recursive tree made by I/P O/P method
 	string op2 = op;
-	op2.push_back(ip[0]);
+	op2.push_back(ip[0]);				
 	
-	ip.erase(ip.begin()+0);
-	
-	solve(ip, op1);
-	solve(ip, op2);
+	ip.erase(ip.begin()+0);				//smaller input
+		
+	solve(ip, op1);					//recursive calls
+	solve(ip, op2);					//recursive calls
 }
 
 int main(){
